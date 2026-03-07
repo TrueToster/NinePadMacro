7ff3405195a3e9ee3224b7af604f1e55ef24cf08
danbro <qiq1488qiq@gmail.com>
2026-03-07 19:34:13 +0300

    added two themes

--- layer_display_windows/NinePad.cpp
+++ layer_display_windows/NinePad.cpp
@@ -5,23 +5,24 @@
 
 #define _CRT_SECURE_NO_WARNINGS
 
-// Идентификаторы для меню
 #define WM_TRAYICON (WM_USER + 1)
 #define ID_TRAY_EXIT 1001
 #define ID_STYLE_BLACK 1002
-#define ID_STYLE_TRANS 1003
+#define ID_STYLE_TRANS_BLACK 1003
+#define ID_STYLE_WHITE 1004
+#define ID_STYLE_TRANS_WHITE 1005
 
 NOTIFYICONDATA nid = {};
 HWND hwnd = NULL;
 bool g_Running = true;
-bool g_useTransparent = false;
+int g_currentTheme = false;
 int g_currentLayer = 0;      
 
 void SaveSettings() {
     HKEY hKey;
     if (RegCreateKeyExA(HKEY_CURRENT_USER, "Software\\NinePad", 0, NULL, 0, KEY_WRITE, NULL, &hKey, NULL) == ERROR_SUCCESS) {
-        DWORD val = g_useTransparent ? 1 : 0;
-        RegSetValueExA(hKey, "TransparentStyle", 0, REG_DWORD, (BYTE*)&val, sizeof(val));
+        DWORD val = g_currentTheme;
+        RegSetValueExA(hKey, "Theme", 0, REG_DWORD, (BYTE*)&val, sizeof(val));
         RegCloseKey(hKey);
     }
 }
@@ -31,8 +32,8 @@ void LoadSettings() {
     if (RegOpenKeyExA(HKEY_CURRENT_USER, "Software\\NinePad", 0, KEY_READ, &hKey) == ERROR_SUCCESS) {
         DWORD val = 0;
         DWORD size = sizeof(val);
-        if (RegQueryValueExA(hKey, "TransparentStyle", NULL, NULL, (BYTE*)&val, &size) == ERROR_SUCCESS) {
-            g_useTransparent = (val == 1);
+        if (RegQueryValueExA(hKey, "Theme", NULL, NULL, (BYTE*)&val, &size) == ERROR_SUCCESS) {
+            g_currentTheme = val;
         }
         RegCloseKey(hKey);
     }
@@ -63,8 +64,8 @@ HICON CreateNumberIconBlack(int number) {
     return hIcon;
 }
 
-// transparent background
-HICON CreateNumberIconTrans(int number) {
+// transparent background, black nums
+HICON CreateNumberIconTransBlack(int number) {
     HDC hdc = GetDC(NULL);
     HDC hdcMem = CreateCompatibleDC(hdc);
 
@@ -126,8 +127,106 @@ HICON CreateNumberIconTrans(int number) {
     return hIcon;
 }
 
+// white background
+HICON CreateNumberIconWhite(int number) {
+    HDC hdc = GetDC(NULL);
+    HDC hdcMem = CreateCompatibleDC(hdc);
+    HBITMAP hbm = CreateCompatibleBitmap(hdc, 16, 16);
+    SelectObject(hdcMem, hbm);
+    
+    RECT rect = {0, 0, 16, 16};
+    HBRUSH hBrush = CreateSolidBrush(RGB(255, 255, 255)); 
+    FillRect(hdcMem, &rect, hBrush);
+    DeleteObject(hBrush);
+    
+    SetBkMode(hdcMem, TRANSPARENT);
+    SetTextColor(hdcMem, RGB(0, 0, 0));
+    char text[2] = {(char)('0' + number), 0};
+    DrawTextA(hdcMem, text, -1, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+    
+    ICONINFO ii = {0};
+    ii.fIcon = TRUE;
+    ii.hbmColor = hbm;
+    ii.hbmMask = hbm;
+    HICON hIcon = CreateIconIndirect(&ii);
+    
+    DeleteDC(hdcMem);
+    DeleteObject(hbm);
+    ReleaseDC(NULL, hdc);
+    return hIcon;
+}
+
+// transparent background, white numbers
+HICON CreateNumberIconTransWhite(int number) {
+    HDC hdc = GetDC(NULL);
+    HDC hdcMem = CreateCompatibleDC(hdc);
+
+    BITMAPV5HEADER bi = {0};
+    bi.bV5Size = sizeof(BITMAPV5HEADER);
+    bi.bV5Width = 16;
+    bi.bV5Height = -16;  
+    bi.bV5Planes = 1;
+    bi.bV5BitCount = 32;
+    bi.bV5Compression = BI_RGB;
+    bi.bV5AlphaMask = 0xFF000000; 
+    
+    void* bits = NULL;
+    HBITMAP hbm = CreateDIBSection(hdc, (BITMAPINFO*)&bi, DIB_RGB_COLORS, &bits, NULL, 0);
+    if (!hbm) { ReleaseDC(NULL, hdc); return NULL; }
+    
+    SelectObject(hdcMem, hbm);
+    DWORD* pixel = (DWORD*)bits;
+    for (int i = 0; i < 16 * 16; i++) pixel[i] = 0x00000000; 
+    
+    SetBkMode(hdcMem, TRANSPARENT);
+    SetTextColor(hdcMem, RGB(255, 255, 255)); 
+    char text[2] = {(char)('0' + number), 0};
+    
+    HDC hdcTemp = CreateCompatibleDC(hdc);
+    HBITMAP hbmTemp = CreateCompatibleBitmap(hdc, 16, 16);
+    SelectObject(hdcTemp, hbmTemp);
+    
+    RECT rect = {0, 0, 16, 16};
+    HBRUSH hBrush = CreateSolidBrush(RGB(0, 0, 0));  
+    FillRect(hdcTemp, &rect, hBrush);
+    DeleteObject(hBrush);
+    
+    SetBkMode(hdcTemp, TRANSPARENT);
+    SetTextColor(hdcTemp, RGB(255, 255, 255)); 
+    DrawTextA(hdcTemp, text, -1, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+    
+    for (int y = 0; y < 16; y++) {
+        for (int x = 0; x < 16; x++) {
+            COLORREF color = GetPixel(hdcTemp, x, y);
+            if (color != RGB(0, 0, 0)) {  
+                pixel[y * 16 + x] = (0xFF << 24) | 0xFFFFFF;  
+            }
+        }
+    }
+    
+    DeleteDC(hdcTemp);
+    DeleteObject(hbmTemp);
+    ICONINFO ii = {0};
+    ii.fIcon = TRUE;
+    ii.hbmColor = hbm;
+    HBITMAP hbmMask = CreateBitmap(16, 16, 1, 1, NULL);
+    ii.hbmMask = hbmMask;
+    HICON hIcon = CreateIconIndirect(&ii);
+    DeleteDC(hdcMem);
+    DeleteObject(hbm);
+    DeleteObject(hbmMask);
+    ReleaseDC(NULL, hdc);
+    return hIcon;
+}
+
 HICON CreateNumberIcon(int number) {
-    return g_useTransparent ? CreateNumberIconTrans(number) : CreateNumberIconBlack(number);
+    switch(g_currentTheme) {
+        case 0: return CreateNumberIconBlack(number);
+        case 1: return CreateNumberIconTransBlack(number);
+        case 2: return CreateNumberIconWhite(number);
+        case 3: return CreateNumberIconTransWhite(number);
+        default: return CreateNumberIconBlack(number);
+    }
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
@@ -136,8 +235,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
             POINT cp; GetCursorPos(&cp);
             HMENU hMenu = CreatePopupMenu();
             
-            AppendMenuW(hMenu, MF_STRING | (g_useTransparent ? MF_UNCHECKED : MF_CHECKED), ID_STYLE_BLACK, L"Темная тема");
-            AppendMenuW(hMenu, MF_STRING | (g_useTransparent ? MF_CHECKED : MF_UNCHECKED), ID_STYLE_TRANS, L"Прозрачная тема");
+            AppendMenuW(hMenu, MF_STRING | (g_currentTheme == 0 ? MF_CHECKED : MF_UNCHECKED), ID_STYLE_BLACK, L"Темная тема");
+            AppendMenuW(hMenu, MF_STRING | (g_currentTheme == 1 ? MF_CHECKED : MF_UNCHECKED), ID_STYLE_TRANS_BLACK, L"Прозрачная тема");
+            AppendMenuW(hMenu, MF_STRING | (g_currentTheme == 2 ? MF_CHECKED : MF_UNCHECKED), ID_STYLE_WHITE, L"Светлая тема");
+            AppendMenuW(hMenu, MF_STRING | (g_currentTheme == 3 ? MF_CHECKED : MF_UNCHECKED), ID_STYLE_TRANS_WHITE, L"Прозрачная светлая тема");
             AppendMenuW(hMenu, MF_SEPARATOR, 0, NULL);
             AppendMenuW(hMenu, MF_STRING, ID_TRAY_EXIT, L"Выход");
             
@@ -149,8 +250,12 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
     if (message == WM_COMMAND) {
         int id = LOWORD(wParam);
         if (id == ID_TRAY_EXIT) g_Running = false;
-        if (id == ID_STYLE_BLACK || id == ID_STYLE_TRANS) {
-            g_useTransparent = (id == ID_STYLE_TRANS);
+        if (id == ID_STYLE_BLACK) g_currentTheme = 0;
+        if (id == ID_STYLE_TRANS_BLACK) g_currentTheme = 1;
+        if (id == ID_STYLE_WHITE) g_currentTheme = 2;
+        if (id == ID_STYLE_TRANS_WHITE) g_currentTheme = 3;
+        
+        if (id >= ID_STYLE_BLACK || id <= ID_STYLE_TRANS_WHITE) {
             SaveSettings();
             HICON hNew = CreateNumberIcon(g_currentLayer);
             if (hNew) {
